10e74c062fdac9527853ebe32c6df4f4b72443a6
alaamehrem <129167450+alaamehrem@users.noreply.github.com>
2024-07-24 20:27:20 +0300

    Task1_FloorCreator

---
+++ FloorCreator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+
+namespace RevitAPI_Tasks
+{
+    [Transaction(TransactionMode.Manual)]
+    public class FloorCreator : IExternalCommand
+    {
+        /**
+         * Create a list of lines to define the floor boundary
+         * Check if there is more than 2 lines
+         * Flatten the list of lines if needed
+         * Rearrange the points to form a closed curve if needed
+         * Get the Floor Type Id and Level Id
+         * Create the floor
+         * */
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // Create a list of lines to define the floor boundary
+            List<Curve> lines = new List<Curve>()
+            {
+                Line.CreateBound(new XYZ(0, 0, 0), new XYZ(79, 0, 0)),
+                Line.CreateBound(new XYZ(44, 25, 0), new XYZ(13, 25, 0)),
+                Line.CreateBound(new XYZ(13, 40, 0), new XYZ(-8, 40, 0)),
+                Line.CreateBound(new XYZ(55, 34, 0), new XYZ(55, 10, 0)),
+                Line.CreateBound(new XYZ(79,34, 0), new XYZ(55,34, 0)),
+                Line.CreateBound(new XYZ(0,20, 0), new XYZ(0,0, 0)),
+                Line.CreateBound(new XYZ(55,10, 0), new XYZ(44,12, 0)),
+                Line.CreateBound(new XYZ(-8,40, 0), new XYZ(-8,20, 0)),
+                Line.CreateBound(new XYZ(79,0, 0), new XYZ(79,34, 0)),
+                Line.CreateBound(new XYZ(44,12, 0), new XYZ(44,25, 0)),
+                Line.CreateBound(new XYZ(-8,20, 0), new XYZ(0,20, 0)),
+                Line.CreateBound(new XYZ(13,25, 0), new XYZ(13,40, 0))
+            };
+
+            // Check if there is more than 2 lines
+            if (lines.Count < 2)
+            {
+                TaskDialog.Show("Error", "You need at least 2 lines to create a floor");
+                return Result.Failed;
+            }
+
+            // Flatten the list of lines if needed
+            if (!Helpers.IsLinesFlattened(lines))
+            {
+                lines = Helpers.FlattenList(lines);
+            }
+
+            // Rearrange the points to form a closed curve if needed
+            lines = Helpers.RearrangeLines(lines);
+
+            // Get the Floor Type Id and Level Id
+            var floorId = new FilteredElementCollector(doc).OfClass(typeof(FloorType)).First().Id;
+            var levelId = new FilteredElementCollector(doc).OfClass(typeof(Level)).First().Id;
+
+            // Create the floor 
+            if (lines.Any())
+            {
+                CurveLoop curveLoop = CurveLoop.Create(lines);
+                List<CurveLoop> profiles = new List<CurveLoop>() { curveLoop };
+                using (Transaction transaction = new Transaction(doc, "Create Floor"))
+                {
+                    transaction.Start();
+                    Floor.Create(doc, profiles, floorId , levelId);
+                    transaction.Commit();
+                    return Result.Succeeded;
+                }
+
+            }
+            else
+            {
+                TaskDialog.Show("Error", "The lines do not form a closed loop");
+                return Result.Failed;
+            }
+        }
+    }
+}
---
+++ Helpers.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace RevitAPI_Tasks
+{
+    public class Helpers
+    {
+        static public bool IsLinesFlattened(List<Curve> lines)
+        {
+            foreach (var line in lines)
+            {
+                if (line.GetEndPoint(0).Z != 0 || line.GetEndPoint(1).Z != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }   
+        static public List<Curve> FlattenList(List<Curve> lines)
+        {
+            List<Curve> flattenedList = new List<Curve>();
+            foreach (var line in lines)
+            {
+                var startpt = line.GetEndPoint(0);
+                var endpt = line.GetEndPoint(1);
+                flattenedList.Add(Line.CreateBound(new XYZ(startpt.X, startpt.Y, 0), new XYZ(endpt.X, endpt.Y, 0)));
+            }
+            return flattenedList;
+        }
+
+        static public List<Curve> RearrangeLines(List<Curve> lines)
+        {
+            List<Curve> rearrangedList = new List<Curve> { lines[0] };
+            lines.RemoveAt(0);
+            while (lines.Count > 0)
+            {
+                bool isFound = false;
+                var endpt1 = rearrangedList[rearrangedList.Count - 1].GetEndPoint(1);
+                for (int j = 0; j < lines.Count; j++)
+                {
+                    var startpt = lines[j].GetEndPoint(0);
+                    if (endpt1.DistanceTo(startpt) == 0)
+                    {
+                        rearrangedList.Add(lines[j]);
+                        lines.RemoveAt(j);
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (!isFound)
+                {
+                    return new List<Curve>();
+                }
+            }
+            return rearrangedList;
+        }
+
+    }
+}
